76cbe40eabbd841cdc3861a77a19b274525f60e3
KostyalBalint <kostyal.balintnak@gmail.com>
2023-05-13 19:38:45 +0200

    feat: fe env variable for netlify

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -3,6 +3,8 @@ import { env } from "./utils/env";
 fetch("/config.json")
   .then((response) => response.json())
   .then(({ REMOTE_DOMAINS, ...config }) => {
-    env.REACT_APP_GRAPHQL_ENDPOINT = config.REACT_APP_GRAPHQL_ENDPOINT;
+    env.REACT_APP_GRAPHQL_ENDPOINT =
+      process.env.REACT_APP_GRAPHQL_ENDPOINT ??
+      config.REACT_APP_GRAPHQL_ENDPOINT;
   })
   .then(() => import("./bootstrap"));
