55b34186b5a4dcdafcedd33dae20892970bc32ee
Oliver Raisz <raiszoliver@gmail.com>
2023-05-23 23:51:05 +0200

    1 test is working correctly

--- frontend/src/tests/AddPlant.test.js
+++ frontend/src/tests/AddPlant.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { AddPlant } from "../components/AddPlant";
 import userEvent from "@testing-library/user-event";
@@ -9,49 +9,55 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-// it("renders AddPlant component correctly", () => {
-//   render(
-//     <ApolloProvider client={client}>
-//       <AddPlant />
-//     </ApolloProvider>
-//   );
+it("renders AddPlant component correctly", () => {
+  render(
+    <ApolloProvider client={client}>
+      <AddPlant />
+    </ApolloProvider>
+  );
 
-//   // Check if the AddIcon is rendered
-//   const addIcon = screen.getByLabelText("Call Segun");
-//   expect(addIcon).toBeInTheDocument();
+  // Check if the AddIcon is rendered
+  const addIcon = screen.getByLabelText("Call Segun");
+  expect(addIcon).toBeInTheDocument();
 
-//   // Check if the PlantModal is initially not visible
-//   const plantModal = screen.queryByText("Add new plant");
-//   expect(plantModal).not.toBeInTheDocument();
-// });
+  // Check if the PlantModal is initially not visible
+  const plantModal = screen.queryByText("Add new plant");
+  expect(plantModal).not.toBeInTheDocument();
+});
 
 it("is able to add a plant correctly", async () => {
-  await render(
+  render(
     <ApolloProvider client={client}>
       <AddPlant />
     </ApolloProvider>
   );
 
   // Click the IconButton to open the PlantModal
-  await fireEvent.click(screen.getByLabelText("Call Segun"));
+  userEvent.click(screen.getByLabelText("Call Segun"));
 
   // Check if the PlantModal is now visible
-  expect(screen.getByText("Add new plant")).toBeVisible();
-
+  await waitFor(() => {
+    expect(screen.getByText("Add new plant")).toBeVisible();
+  });
   //gets the Name and Watering Frequency in Days textboxes
-  const nameTb = screen.getByPlaceholderText(/Name/);
-  const wateringFrequencyTb = screen.getByPlaceholderText(/Watering/);
+  const nameTb = screen.getByPlaceholderText("Name");
+  const wateringFrequencyTb = screen.getByPlaceholderText(
+    "Watering Frequency in Days"
+  );
   expect(nameTb).toBeVisible();
   expect(wateringFrequencyTb).toBeVisible();
-
   //puts "testplant" and 10 into the textboxes
-  await userEvent.type(nameTb, "Ubul, a növény");
-  await userEvent.type(wateringFrequencyTb, 10);
+  let plantName = "Ubul, a növény";
+  userEvent.type(nameTb, plantName);
+  userEvent.type(wateringFrequencyTb, 10);
 
-  //fireEvent.click(screen.getByText(/Add/));
+  userEvent.click(screen.getByText("Add"));
+  await waitFor(() => {
+    expect(screen.getByText(plantName)).toBeInTheDocument();
+  });
 });
 
-// it("'last watered' should update when plant is watered", () => {});
+//it("'last watered' should update when plant is watered", () => {});
 
 // it("water frequency should be equal to the value given at edit", () => {});
 
