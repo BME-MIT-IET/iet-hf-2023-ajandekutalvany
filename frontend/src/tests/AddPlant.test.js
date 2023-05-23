b36bc276a95a7fb8b7fece3de38266e66b41f74d
Oliver Raisz <raiszoliver@gmail.com>
2023-05-23 20:58:53 +0200

    added skeletons of ui tests

--- frontend/src/tests/AddPlant.test.js
+++ frontend/src/tests/AddPlant.test.js
@@ -2,74 +2,57 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { AddPlant } from "../components/AddPlant";
+import userEvent from "@testing-library/user-event";
 
 const client = new ApolloClient({
   // Configure your ApolloClient instance here
   cache: new InMemoryCache(),
 });
 
-describe("AddPlant component", () => {
-  test("renders AddPlant component correctly", () => {
-    render(
-      <ApolloProvider client={client}>
-        <AddPlant />
-      </ApolloProvider>
-    );
+// it("renders AddPlant component correctly", () => {
+//   render(
+//     <ApolloProvider client={client}>
+//       <AddPlant />
+//     </ApolloProvider>
+//   );
 
-    // Check if the AddIcon is rendered
-    const addIcon = screen.getByLabelText("Call Segun");
-    expect(addIcon).toBeInTheDocument();
+//   // Check if the AddIcon is rendered
+//   const addIcon = screen.getByLabelText("Call Segun");
+//   expect(addIcon).toBeInTheDocument();
 
-    // Check if the PlantModal is initially not visible
-    const plantModal = screen.queryByText("Add new plant");
-    expect(plantModal).not.toBeInTheDocument();
+//   // Check if the PlantModal is initially not visible
+//   const plantModal = screen.queryByText("Add new plant");
+//   expect(plantModal).not.toBeInTheDocument();
+// });
 
-    // Click the IconButton to open the PlantModal
-    fireEvent.click(addIcon);
+it("is able to add a plant correctly", async () => {
+  await render(
+    <ApolloProvider client={client}>
+      <AddPlant />
+    </ApolloProvider>
+  );
 
-    // Check if the PlantModal is now visible
-    const openedPlantModal = screen.getByText("Add new plant");
-    expect(openedPlantModal).toBeInTheDocument();
-  });
+  // Click the IconButton to open the PlantModal
+  await fireEvent.click(screen.getByLabelText("Call Segun"));
 
-  test("handles form submission correctly", () => {
-    // Mock the useAddPlantMutation hook implementation
-    const mockAddPlantMutation = jest.fn();
-    jest.mock("../generated/graphql", () => ({
-      useAddPlantMutation: () => [mockAddPlantMutation],
-    }));
+  // Check if the PlantModal is now visible
+  expect(screen.getByText("Add new plant")).toBeVisible();
 
-    render(
-      <ApolloProvider client={client}>
-        <AddPlant />
-      </ApolloProvider>
-    );
+  //gets the Name and Watering Frequency in Days textboxes
+  const nameTb = screen.getByPlaceholderText(/Name/);
+  const wateringFrequencyTb = screen.getByPlaceholderText(/Watering/);
+  expect(nameTb).toBeVisible();
+  expect(wateringFrequencyTb).toBeVisible();
 
-    // Click the IconButton to open the PlantModal
-    const addIcon = screen.getByLabelText("Call Segun");
-    fireEvent.click(addIcon);
+  //puts "testplant" and 10 into the textboxes
+  await userEvent.type(nameTb, "Ubul, a növény");
+  await userEvent.type(wateringFrequencyTb, 10);
 
-    //  Enter values in the form fields
-    const nameInput = screen.getByLabelText("Name");
-    fireEvent.change(nameInput, { target: { value: "Plant A" } });
-
-    const waterFrequencyInput = screen.getByLabelText("Watering Frequency");
-    fireEvent.change(waterFrequencyInput, { target: { value: "2" } });
-
-    // Submit the form
-    const submitButton = screen.getByText("Add");
-    fireEvent.click(submitButton);
-
-    // Verify that the addPlant mutation was called with the correct variables
-    expect(mockAddPlantMutation).toHaveBeenCalledWith({
-      variables: {
-        name: "Plant A",
-        waterFrequency: 2,
-      },
-    });
-
-    /** Verify that the PlantModal is closed
-    const plantModal = screen.queryByText("Add new plant");
-    expect(plantModal).not.toBeInTheDocument();*/
-  });
+  //fireEvent.click(screen.getByText(/Add/));
 });
+
+// it("'last watered' should update when plant is watered", () => {});
+
+// it("water frequency should be equal to the value given at edit", () => {});
+
+// it("deleted plant should not be visible", () => {});
