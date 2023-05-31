44253a3f2b4305ef74947f3b7d1a73d09e15befb
Loznek <kenzio4444@gmail.com>
2023-05-31 21:44:48 +0200

    Create performance tests for editing plant

---
+++ backend/test/performance_tests/edit_plant/edit_plant_spike.ts
@@ -0,0 +1,41 @@
+import http from 'k6/http';
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 100 },
+    { duration: '10s', target: 200 },
+    { duration: '10s', target: 2000 },
+    { duration: '10s', target: 2000 },
+    { duration: '10s', target: 400 },
+    { duration: '10s', target: 400 },
+    { duration: '10s', target: 0 },
+  ],
+};
+
+const addr = 'https://anton.sch.bme.hu/iet/api/graphql//:3001/graphql';
+
+/**
+ * The id must be a valid id of a plant in the database.
+ */
+const query = `
+ mutation editPlant{
+    editPlant(id: "53576c55-2072-482f-8a95-2eebb1553ca4", plant: { name: "perf_test_modified", waterFrequency: 2 }) {
+        id,
+            name
+    }
+}
+`;
+
+const headers = {
+  'Content-Type': 'application/json',
+};
+
+export default function () {
+  http.post(
+    addr,
+    JSON.stringify({
+      query,
+    }),
+    { headers },
+  );
+}
