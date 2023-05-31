e103427d6286a4d7ba32e8a4f018190c6afa3256
Loznek <kenzio4444@gmail.com>
2023-05-31 21:52:36 +0200

    Create performance tests for watering a plant

---
+++ backend/test/performance_tests/query_plants/query_plants_stress.ts
@@ -0,0 +1,42 @@
+import http from 'k6/http';
+
+export const options = {
+  stages: [
+    { duration: '30s', target: 200 },
+    { duration: '30s', target: 400 },
+    { duration: '30s', target: 800 },
+    { duration: '30s', target: 1200 },
+    { duration: '30s', target: 1200 },
+    { duration: '30s', target: 800 },
+    { duration: '30s', target: 400 },
+    { duration: '30s', target: 200 },
+    { duration: '30s', target: 0 },
+  ],
+};
+
+const addr = 'https://anton.sch.bme.hu/iet/api/graphql//:3001/graphql';
+
+const query = `
+   query plants {
+    plants {
+        id
+        name
+        lastWatered
+        waterFrequency
+    }
+}
+`;
+
+const headers = {
+  'Content-Type': 'application/json',
+};
+
+export default function () {
+  http.post(
+    addr,
+    JSON.stringify({
+      query,
+    }),
+    { headers },
+  );
+}
---
+++ backend/test/performance_tests/water_plant/water_plant_load.ts
@@ -0,0 +1,35 @@
+import http from 'k6/http';
+
+export const options = {
+  stages: [
+    { duration: '20s', target: 75 },
+    { duration: '20s', target: 150 },
+    { duration: '20s', target: 75 },
+    { duration: '20s', target: 0 },
+  ],
+};
+
+const addr = 'https://anton.sch.bme.hu/iet/api/graphql//:3001/graphql';
+
+/**
+ * The id must be a valid id of a plant in the database.
+ */
+const query = `
+  mutation waterPlant {
+    waterPlant(id: "53576c55-2072-482f-8a95-2eebb1553ca4")
+}
+`;
+
+const headers = {
+  'Content-Type': 'application/json',
+};
+
+export default function () {
+  http.post(
+    addr,
+    JSON.stringify({
+      query,
+    }),
+    { headers },
+  );
+}
